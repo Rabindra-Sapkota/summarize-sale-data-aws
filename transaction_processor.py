ac5898e68cf3230cd4f77c9d98ecb55fc30400e6
Rabindra Sapkota <rabindra.sapkota@pdc01.lisnepal.com.np>
2022-02-25 19:28:53 +0545

    Print statement added for proper logging

--- transaction_processor.py
+++ transaction_processor.py
@@ -2,10 +2,16 @@ import constants
 import pymysql
 import os
 
-DATABASE_HOST = os.environ['DATABASE_HOST']
-DATABASE_USER = os.environ['DATABASE_USER']
-DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
-DATABASE_NAME = os.environ['DATABASE_NAME']
+
+print("Getting Values from environment variable")
+try:
+    DATABASE_HOST = os.environ['DATABASE_HOST']
+    DATABASE_USER = os.environ['DATABASE_USER']
+    DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+    DATABASE_NAME = os.environ['DATABASE_NAME']
+except Exception as error:
+    print("Exception occurred while getting environment variable")
+    print(error)
 
 
 def summarize_transaction(transaction_data, file_name):
@@ -16,7 +22,7 @@ def summarize_transaction(transaction_data, file_name):
         record_count = record_count + 1
         product_name, quantity, amount, revenue = transaction.split(',')
         if product_name in summary_data.keys():
-            current_quantity, current_amount, current_revenue = summary_data[product_name]
+            current_quantity, current_amount, current_revenue = summary_data.get(product_name)
             summary_data[product_name] = current_quantity + int(quantity), current_amount + float(amount), \
                                          current_revenue + float(revenue)
         else:
@@ -29,6 +35,7 @@ def summarize_transaction(transaction_data, file_name):
 
 
 def generate_sql_query(transaction_list):
+    print("Generating SQL query")
     column_names = constants.RDS_COLUMN_HEADERS
     data = str(transaction_list).strip('[]')
     sql_query = f'INSERT INTO {constants.TABLE_NAME}({column_names}) VALUES {data};'
@@ -37,9 +44,10 @@ def generate_sql_query(transaction_list):
 
 def insert_to_rds(summarized_transaction):
     sql_query = generate_sql_query(summarized_transaction)
+    print("Obtaining database connection")
     connection = pymysql.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWORD, db=DATABASE_NAME)
 
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(sql_query)
-        connection.commit()
+    with connection.cursor() as cursor:
+        print("Executing SQL query")
+        cursor.execute(sql_query)
+    connection.commit()
